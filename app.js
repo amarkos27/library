e4b6f56f8a8af2b10c0d713504fccd83f6238127
amarkos27 <armarkos27@gmail.com>
2023-08-01 16:41:52 -0400

    Implement app using factory functions

--- app.js
+++ app.js
@@ -1,149 +1,114 @@
 const books = document.querySelector('.books');
 const library = [];
 const add = document.querySelector('.add');
-const overlay = document.querySelector('.modal-overlay');
-const modal = document.querySelector('.hide-modal');
-const submit = document.querySelector('#sub');
 
-const resetModal = () => {
-  const form = modal.childNodes[1];
-  const inputs = modal.querySelectorAll('input');
-  form.reset();
-  inputs.forEach((input) => input.classList.remove('invalid'));
+const card = (title, author, pages, read) => {
+  const newBook = { title, author, pages, read };
+  const container = document.createElement('div');
+  const items = {
+    remove: document.createElement('i'),
+    title: document.createElement('h1'),
+    author: document.createElement('p'),
+    pages: document.createElement('p'),
+    read: document.createElement('read'),
+  };
+
+  container.classList.add('card');
+  items.remove.classList.add('remove');
+  items.read.classList.add('btn');
+
+  const isRead = (read) => {
+    if (read) {
+      items.read.textContent = 'Read';
+      items.read.classList.remove('not-read');
+      items.read.classList.add('read');
+    } else {
+      items.read.classList.remove('read');
+      items.read.classList.add('not-read');
+      items.read.textContent = 'Not Read';
+    }
+  };
+
+  items.read.addEventListener('click', () => {
+    if (items.read.textContent === 'Read') {
+      // If the button is being clicked when it already says read,
+      // the book now switches to not read, and vice versa
+      isRead(false);
+    } else isRead(true);
+  });
+
+  const fillCard = () => {
+    items.title.textContent = newBook.title;
+    items.author.textContent = `Author:\n${newBook.author}`;
+    items.pages.textContent = `Pages:\n${newBook.pages}`;
+    isRead(newBook.read);
+    Object.values(items).forEach((item) => container.appendChild(item));
+
+    library.push(newBook);
+  };
+
+  return container;
 };
 
-const closeModal = (e) => {
-  if (
-    e.target.classList.contains('modal-overlay') ||
-    e.target.classList.contains('btn')
-  ) {
+const entry = () => {
+  const modal = document.querySelector('.hide-modal');
+  const overlay = document.querySelector('.modal-overlay');
+  const inputs = Array.from(modal.querySelectorAll('input'));
+  const submit = modal.querySelector('.btn');
+
+  modal.addEventListener('click', (e) => {
+    e.stopPropagation();
+  });
+
+  const resetForm = () => {
+    const form = modal.querySelector('form');
+    form.reset();
+  };
+
+  const openModal = () => {
+    overlay.classList.add('show');
+    modal.classList.remove('hide-modal');
+    modal.classList.add('modal');
+  };
+
+  const closeModal = () => {
     modal.classList.add('hide-modal');
-    resetModal(); // Reset form when modal is closed
+    inputs.forEach((input) => input.classList.remove('invalid'));
+    resetForm();
     setTimeout(() => {
       overlay.classList.remove('show');
     }, 250);
-  }
-};
+  };
 
-const openModal = () => {
-  overlay.classList.add('show');
-  modal.classList.remove('hide-modal');
-  modal.classList.add('modal');
-};
+  const submitBook = (e) => {
+    e.preventDefault();
+    let valid = true;
 
-add.addEventListener('click', () => {
-  openModal();
-});
+    inputs.forEach((input) => {
+      if (!input.checkValidity()) {
+        input.classList.add('invalid');
+        input.reportValidity();
+        valid = false;
+      }
+    });
 
-overlay.addEventListener('click', (e) => {
-  closeModal(e);
-});
-
-function Book(title, author, pages, read) {
-  this.title = title;
-  this.author = author;
-  this.pages = pages;
-  this.read = read;
-}
-
-Book.prototype.createCard = function () {
-  const card = {};
-  const container = document.createElement('div');
-  card.remove = document.createElement('i');
-  card.title = document.createElement('h1');
-  card.author = document.createElement('p');
-  card.pages = document.createElement('p');
-  card.read = document.createElement('button');
-
-  card.read.addEventListener('click', () => {
-    card.read.classList.toggle('read');
-    card.read.classList.toggle('not-read');
-    if (card.read.classList.contains('read')) {
-      card.read.textContent = 'Read';
-      this.read = true;
-    } else {
-      card.read.textContent = 'Not Read';
-      this.read = false;
+    if (valid) {
+      const newCard = card(
+        inputs[0].value,
+        inputs[1].value,
+        inputs[2].value,
+        inputs[3].checked
+      );
+      books.appendChild(newCard);
     }
-  });
+  };
 
-  card.remove.addEventListener('click', () => {
-    books.removeChild(container);
-    const index = library.indexOf(this);
-    library.splice(index, 1);
-  });
-
-  container.classList.add('card');
-  card.remove.classList.add('remove');
-  card.read.classList.add('btn');
-  card.title.textContent = this.title;
-  card.author.textContent = `Author:\n${this.author}`;
-  card.pages.textContent = `Pages:\n${this.pages}`;
-
-  if (this.read) {
-    card.read.classList.add('read');
-    card.read.textContent = 'Read';
-  } else {
-    card.read.classList.add('not-read');
-    card.read.textContent = 'Not Read';
-  }
-
-  Object.values(card).forEach((value) => {
-    container.appendChild(value);
-  });
-
-  books.appendChild(container);
-};
-
-const checkValid = (...args) => {
-  let valid = true;
-  const inputs = [...args];
-
-  inputs.forEach((input) => {
-    if (!input.checkValidity()) {
-      valid = false;
-    } else {
-      input.classList.remove('invalid');
-    }
-  });
-
-  return valid;
-};
-
-const validateForm = (...args) => {
-  const inputs = [...args];
-  inputs.forEach((input) => {
-    if (!input.checkValidity()) {
-      input.classList.add('invalid');
-      input.reportValidity();
-    }
+  add.addEventListener('click', openModal);
+  overlay.addEventListener('click', closeModal);
+  submit.addEventListener('click', (e) => {
+    submitBook(e);
+    closeModal();
   });
 };
 
-const submitBook = (title, author, pages, read) => {
-  const newBook = new Book(
-    title.value,
-    author.value,
-    pages.value,
-    read.checked
-  );
-  library.push(newBook);
-};
-
-submit.addEventListener('click', (e) => {
-  e.preventDefault();
-  e.stopPropagation();
-  const title = document.querySelector('#title');
-  const author = document.querySelector('#author');
-  const pages = document.querySelector('#pages');
-  const read = document.querySelector('#complete');
-
-  const formValid = checkValid(title, author, pages);
-  if (formValid) {
-    submitBook(title, author, pages, read);
-    closeModal(e);
-    library[library.length - 1].createCard();
-  } else {
-    validateForm(title, author, pages);
-  }
-});
+entry();
