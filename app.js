4fa4294bb64383081a3dec8944b59a8cc1c1d4be
amarkos27 <armarkos27@gmail.com>
2023-08-02 19:00:11 -0400

    Re-implement the project using factory functions
    Previously, this project was written using standard constructor
    functions. Replace them with factories.

--- app.js
+++ app.js
@@ -1,114 +1,121 @@
 const books = document.querySelector('.books');
 const library = [];
 const add = document.querySelector('.add');
+const modal = document.querySelector('.hide-modal');
+const overlay = document.querySelector('.modal-overlay');
+const inputs = Array.from(modal.querySelectorAll('input'));
+const submit = modal.querySelector('.btn');
 
-const card = (title, author, pages, read) => {
-  const newBook = { title, author, pages, read };
+const card = () => {
   const container = document.createElement('div');
-  const items = {
-    remove: document.createElement('i'),
-    title: document.createElement('h1'),
-    author: document.createElement('p'),
-    pages: document.createElement('p'),
-    read: document.createElement('read'),
-  };
+  const remove = document.createElement('i');
+  const title = document.createElement('h1');
+  const author = document.createElement('p');
+  const pages = document.createElement('p');
+  const read = document.createElement('button');
 
   container.classList.add('card');
-  items.remove.classList.add('remove');
-  items.read.classList.add('btn');
+  remove.classList.add('remove');
+  read.classList.add('btn');
 
-  const isRead = (read) => {
-    if (read) {
-      items.read.textContent = 'Read';
-      items.read.classList.remove('not-read');
-      items.read.classList.add('read');
+  return { container, remove, title, author, pages, read };
+};
+
+const book = (title, author, pages, read) => {
+  const newBook = { title, author, pages, read };
+  const newCard = card();
+  const fields = Object.values(newCard).slice(1);
+
+  newCard.remove.addEventListener('click', () => {
+    books.removeChild(newCard.container);
+
+    const index = library.indexOf(newBook);
+    library.splice(index, 1);
+  });
+
+  newCard.read.addEventListener('click', () => {
+    const index = library.indexOf(newBook);
+
+    if (newCard.read.textContent === 'Read') {
+      newCard.read.classList.remove('read');
+      newCard.read.classList.add('not-read');
+      newCard.read.textContent = 'Not Read';
+      library[index].read = false;
     } else {
-      items.read.classList.remove('read');
-      items.read.classList.add('not-read');
-      items.read.textContent = 'Not Read';
+      newCard.read.classList.remove('not-read');
+      newCard.read.classList.add('read');
+      newCard.read.textContent = 'Read';
+      library[index].read = true;
     }
-  };
-
-  items.read.addEventListener('click', () => {
-    if (items.read.textContent === 'Read') {
-      // If the button is being clicked when it already says read,
-      // the book now switches to not read, and vice versa
-      isRead(false);
-    } else isRead(true);
   });
 
-  const fillCard = () => {
-    items.title.textContent = newBook.title;
-    items.author.textContent = `Author:\n${newBook.author}`;
-    items.pages.textContent = `Pages:\n${newBook.pages}`;
-    isRead(newBook.read);
-    Object.values(items).forEach((item) => container.appendChild(item));
+  newCard.title.textContent = title;
+  newCard.author.textContent = `Author:\n${author}`;
+  newCard.pages.textContent = `Pages:\n${pages}`;
+  if (read) {
+    newCard.read.classList.add('read');
+    newCard.read.textContent = 'Read';
+  } else {
+    newCard.read.classList.add('not-read');
+    newCard.read.textContent = 'Not Read';
+  }
 
-    library.push(newBook);
-  };
-
-  return container;
+  fields.forEach((field) => newCard.container.appendChild(field));
+  return { newBook, newCard };
 };
 
-const entry = () => {
-  const modal = document.querySelector('.hide-modal');
-  const overlay = document.querySelector('.modal-overlay');
-  const inputs = Array.from(modal.querySelectorAll('input'));
-  const submit = modal.querySelector('.btn');
+// Modal interaction
+modal.addEventListener('click', (e) => {
+  e.stopPropagation();
+});
 
-  modal.addEventListener('click', (e) => {
-    e.stopPropagation();
+const resetForm = () => {
+  const form = modal.querySelector('form');
+  form.reset();
+};
+
+const openModal = () => {
+  overlay.classList.add('show');
+  modal.classList.remove('hide-modal');
+  modal.classList.add('modal');
+};
+
+const closeModal = () => {
+  modal.classList.add('hide-modal');
+  inputs.forEach((input) => input.classList.remove('invalid'));
+  resetForm();
+  setTimeout(() => {
+    overlay.classList.remove('show');
+  }, 250);
+};
+
+const submitBook = (e) => {
+  e.preventDefault();
+  let valid = true;
+
+  inputs.forEach((input) => {
+    if (!input.checkValidity()) {
+      input.classList.add('invalid');
+      input.reportValidity();
+      valid = false;
+    }
   });
 
-  const resetForm = () => {
-    const form = modal.querySelector('form');
-    form.reset();
-  };
-
-  const openModal = () => {
-    overlay.classList.add('show');
-    modal.classList.remove('hide-modal');
-    modal.classList.add('modal');
-  };
-
-  const closeModal = () => {
-    modal.classList.add('hide-modal');
-    inputs.forEach((input) => input.classList.remove('invalid'));
-    resetForm();
-    setTimeout(() => {
-      overlay.classList.remove('show');
-    }, 250);
-  };
-
-  const submitBook = (e) => {
-    e.preventDefault();
-    let valid = true;
-
-    inputs.forEach((input) => {
-      if (!input.checkValidity()) {
-        input.classList.add('invalid');
-        input.reportValidity();
-        valid = false;
-      }
-    });
-
-    if (valid) {
-      const newCard = card(
-        inputs[0].value,
-        inputs[1].value,
-        inputs[2].value,
-        inputs[3].checked
-      );
-      books.appendChild(newCard);
-    }
-  };
-
-  add.addEventListener('click', openModal);
-  overlay.addEventListener('click', closeModal);
-  submit.addEventListener('click', (e) => {
-    submitBook(e);
+  if (valid) {
+    const entry = book(
+      inputs[0].value,
+      inputs[1].value,
+      inputs[2].value,
+      inputs[3].checked
+    );
+    library.push(entry.newBook);
+    books.appendChild(entry.newCard.container);
     closeModal();
-  });
+  }
 };
 
-entry();
+add.addEventListener('click', openModal);
+overlay.addEventListener('click', closeModal);
+submit.addEventListener('click', (e) => {
+  submitBook(e);
+});
